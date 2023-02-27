ef401153fcf853cfb42afaabc9826570b8e7faab
khaledbohout2 <46874509+khaledbohout2@users.noreply.github.com>
2023-02-27 20:42:41 +0200

    Initial Commit

---
+++ GamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamesApp
+//
+//  Created by Khaled Bohout on 27/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
